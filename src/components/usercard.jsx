5a4452c4f8ff61650726da54329e5e217cdb1d47
gitsibi <git@stackblitz.com>
2025-02-28 12:31:07 +0530

    Initial commit

---
+++ src/components/usercard.jsx
@@ -0,0 +1,23 @@
+// write the component code here
+
+function userCard()
+{
+   return(
+     <div>
+         <img src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg" alt="">
+           <h2>Sibishree</h2>
+           <p>
+        <strong>Email: </strong>
+         sibishree200@gmail.com
+      </p>
+      <p>
+        <strong>Phone: </strong>
+        xxxxxxxxx
+      </p>
+      <p>
+        <strong>Address: </strong>
+        123 Main St, Anytown, USA
+      </p>
+     </div>
+   )
+}
